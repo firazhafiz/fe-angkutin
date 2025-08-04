ec5c041d31f6463832d34e241cc2f2f2707cc1d0
firazhafiz <firazfulvianhafiz05@gmail.com>
2025-08-04 16:39:54 +0700

    fix params detail blog

--- src/app/(main)/blog/[id]/page.tsx
+++ src/app/(main)/blog/[id]/page.tsx
@@ -1,10 +1,16 @@
 import DetailBlog from "../../../../../components/organisms/DetailBlog";
 
-export default function BlogDetail({ params }: { params: { id: string } }) {
+export default async function BlogDetail({
+  params,
+}: {
+  params: Promise<{ id: string }>;
+}) {
+  const resolvedParams = await params;
+
   return (
     <main className="w-full relative min-h-screen bg-gray-100 overflow-hidden">
       <section className="max-w-6xl mx-auto mt-40 mb-20">
-        <DetailBlog params={params} />
+        <DetailBlog params={resolvedParams} />
       </section>
     </main>
   );
