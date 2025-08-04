180b5f3a0c895129005bb54a287176bfe53b6e95
firazhafiz <firazfulvianhafiz05@gmail.com>
2025-08-04 15:53:56 +0700

    fix skeleton blog

--- components/atoms/BlogListSkeleton.tsx
+++ components/atoms/BlogListSkeleton.tsx
@@ -4,9 +4,6 @@ export default function BlogListSkeleton() {
   return (
     <section className="w-full">
       <div className="w-full mx-auto">
-        <h2 className="text-2xl md:text-3xl font-bold text-gray-300 mb-12">
-          Recent Blog Posts
-        </h2>
         <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
           {Array(skeletonCount)
             .fill(null)
@@ -16,16 +13,26 @@ export default function BlogListSkeleton() {
                 className="h-full cursor-pointer overflow-hidden bg-gray-200 animate-pulse"
               >
                 {/* Image Skeleton */}
-                <div className="w-full h-[250px] bg-gray-300 rounded-lg"></div>
+                <div className="w-full h-[250px] relative bg-gray-300 rounded-lg"></div>
 
                 {/* Content Skeleton */}
                 <div className="py-4 flex flex-col h-[140px]">
-                  <div className="h-5 bg-gray-300 rounded mb-2"></div>{" "}
-                  {/* Title */}
-                  <div className="h-4 bg-gray-300 rounded mb-3 flex-1"></div>{" "}
-                  {/* Description */}
-                  <div className="h-3 bg-gray-300 rounded w-1/3"></div>{" "}
-                  {/* Author and Date */}
+                  {/* Title Skeleton (text-lg, font-bold) */}
+                  <div className="h-6 bg-gray-300 rounded mb-2"></div>
+
+                  {/* Description Skeleton (text-sm, line-clamp-2, flex-1) */}
+                  <div className="h-4 bg-gray-300 rounded mb-3 flex-1"></div>
+                  <div className="h-4 bg-gray-300 rounded mb-3 flex-1"></div>
+
+                  {/* Author and Date Skeleton (flex, text-xs, font-semibold) */}
+                  <div className="flex items-center text-xs">
+                    <div className="h-3 bg-gray-300 rounded mr-2 w-1/4"></div>{" "}
+                    {/* Author */}
+                    <div className="h-3 bg-gray-300 rounded mx-2 w-2"></div>{" "}
+                    {/* Bullet */}
+                    <div className="h-3 bg-gray-300 rounded w-1/4"></div>{" "}
+                    {/* Date */}
+                  </div>
                 </div>
               </div>
             ))}
